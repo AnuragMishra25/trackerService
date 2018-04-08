e55ebbf8918ab8705fb1c416a38bb0d9e8ea6132
Anurag <anurag.mishra@fundingsocieties.com>
2018-04-08 22:02:04 +0800

    Adding code for basic setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const path = require('path')
+
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello World!!!');
+})
+
+app.listen(3000, function(){
+    console.log("Ndde app started on port 3000!!")
+});
+
