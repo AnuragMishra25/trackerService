240fb00e5a9b93281a46ab665568dd71ec18e87c
Anurag <anurag.mishra@fundingsocieties.com>
2018-04-13 22:46:11 +0800

    Adding CORS

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-Token, X-XSRF-Token, username');
+    next();
+  });
+
 app.use('/', require('./controllers/index'));
 
 
